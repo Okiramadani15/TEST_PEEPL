a795a02884a9e88b0d72c5ef308a38898bf0df2e
oki ramadani <okiramadani691@gmail.com>
2026-03-27 00:09:13 +0700

    [UPDATE] fixing code

--- models/sale_order.py
+++ models/sale_order.py
@@ -2,9 +2,12 @@ from odoo import models, fields
 from odoo.exceptions import ValidationError
 
 class SalesOrder (models.Models):
-    for order in self: 
-        if order.amount_total < 500000:
-            raise ValidationError("Minimal order  value adalah 500.000")
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+       for order in self: 
+          if order.amount_total < 500000:
+              raise ValidationError("Minimal order  value adalah 500.000")
         
 
-    return super (SalesOrder, self).action_confirm()
+          return super (SalesOrder, self).action_confirm()
